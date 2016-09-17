28fc7e13780c155a2876c76c23e8bfa0ecdfa1f9
Wang pei <519875872@qq.com>
2016-09-17 23:57:52 +0800

    Diamond

--- src/main/java/com/twu/exercises/TriangleExercises.java
+++ src/main/java/com/twu/exercises/TriangleExercises.java
@@ -9,14 +9,23 @@ public class TriangleExercises {
         int number = scanner.nextInt();
 
         for (int i = 0; i < number; i++) {
-            for (int j = 0; j < number*2-1; j++) {
-                if(j<=(number+i)-1 && j>=(number-i)-1){
-                    System.out.print("*");
-                }else {
-                    System.out.print(" ");
-                }
-            }
+            drawLine(number, i);
             System.out.println();
+        }
+
+        for (int i = number-2; i >= 0 ; i--) {
+            drawLine(number, i);
+            System.out.println();
+        }
+    }
+
+    private static void drawLine(int number, int i) {
+        for (int j = 0; j < number*2-1; j++) {
+            if(j<=(number+i)-1 && j>=(number-i)-1){
+                System.out.print("*");
+            }else {
+                System.out.print(" ");
+            }
         }
     }
 }
