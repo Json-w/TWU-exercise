1d09d4ff503b6bf86719a56bd72b33ec20a86719
Wang pei <519875872@qq.com>
2016-09-18 00:02:12 +0800

    Diamond with Name

--- src/main/java/com/twu/exercises/TriangleExercises.java
+++ src/main/java/com/twu/exercises/TriangleExercises.java
@@ -8,11 +8,11 @@ public class TriangleExercises {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
 
-        for (int i = 0; i < number; i++) {
+        for (int i = 0; i < number-1; i++) {
             drawLine(number, i);
             System.out.println();
         }
-
+        System.out.println("Wang Pei");
         for (int i = number-2; i >= 0 ; i--) {
             drawLine(number, i);
             System.out.println();
